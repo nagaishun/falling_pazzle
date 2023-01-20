3663b2e9d05b8d06b566e1c0a9cd14dfa3c0afba
STD\s20217035 <20217035-nagaishun@hcs.ac.jp>
2023-01-20 13:51:24 +0900

    テスト用のコードを追記

--- GBoardController.cs
+++ GBoardController.cs
@@ -7,6 +7,8 @@ public class BoardController : MonoBehaviour
     public const int BOARD_WIDTH = 6;
     public const int BOARD_HEIGHT = 14;
 
+    [SerializeField] GameObject prefabPuyo = default!;
+
     int[,] _board = new int[BOARD_HEIGHT, BOARD_WIDTH];
     GameObject[,] _Puyos = new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
 
@@ -30,6 +32,14 @@ public class BoardController : MonoBehaviour
     void Start()
     {
         ClearAll();
+
+        for (int y = 0; y < BOARD_HEIGHT; y++)
+        {
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                Settle(new Vector2Int(x, y), Random.Range(1, 5));
+            }
+        }
     }
 
     public static bool IsValidated(Vector2Int pos)
@@ -53,7 +63,13 @@ public class BoardController : MonoBehaviour
         {
             return false;
         }
+
         _board[pos.y, pos.x] = val;
+
+        Debug.Assert(_Puyos[pos.y, pos.x] == null);
+        Vector3 world_position = transform.position + new Vector3(pos.x, pos.y, 0.0f);
+        _Puyos[pos.y, pos.x] = Instantiate(prefabPuyo, world_position, Quaternion.identity, transform);
+        _Puyos[pos.y, pos.x].GetComponent<Puyo_Controller>().SetPuyoType((PuyoType)val);
 
         return true;
     }
