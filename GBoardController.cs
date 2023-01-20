c6208f9fd187beb301b77b6dd49b322208bf1b77
STD\s20217035 <20217035-nagaishun@hcs.ac.jp>
2023-01-20 13:57:20 +0900

    テスト用のコードを削除

--- GBoardController.cs
+++ GBoardController.cs
@@ -33,13 +33,13 @@ public class BoardController : MonoBehaviour
     {
         ClearAll();
 
-        for (int y = 0; y < BOARD_HEIGHT; y++)
-        {
-            for (int x = 0; x < BOARD_WIDTH; x++)
-            {
-                Settle(new Vector2Int(x, y), Random.Range(1, 5));
-            }
-        }
+        //for (int y = 0; y < BOARD_HEIGHT; y++)
+        //{
+        //    for (int x = 0; x < BOARD_WIDTH; x++)
+        //    {
+        //        Settle(new Vector2Int(x, y), Random.Range(1, 5));
+        //    }
+        //}
     }
 
     public static bool IsValidated(Vector2Int pos)
