b322746a93985862b8b5805df84d4f8eb4be0d01
STD\s20217035 <20217035-nagaishun@hcs.ac.jp>
2023-01-27 14:53:31 +0900

    プルのテスト

--- GPuyoPair.cs
+++ GPuyoPair.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class PuyoPair : MonoBehaviour
 {
     [SerializeField] Puyo_Controller[] puyos = { default!, default! };
+
+    public void SetPuyoType(PuyoType axis, PuyoType child)
+    {
+        puyos[0].SetPuyoType(axis);
+        puyos[1].SetPuyoType(child);
+    }
 }
