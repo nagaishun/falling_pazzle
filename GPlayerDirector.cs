b2b46fd10f07ae9e372ab081f9708b47241e0bd9
STD\s20217035 <20217035-nagaishun@hcs.ac.jp>
2023-01-30 14:29:41 +0900

    GameOverリトライ処理用のコードを追記

--- GPlayerDirector.cs
+++ GPlayerDirector.cs
@@ -1,7 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+interface IState
+{
+    public enum E_State
+    {
+        Control = 0,
+        GameOver = 1,
+
+        MAX,
+
+        Unchanged,
+    }
+
+    E_State Initialize(PlayDirector parent);
+    E_State Update(PlayDirector parent);
+}
 public class PlayDirector : MonoBehaviour
 {
     [SerializeField] GameObject player = default!;
@@ -12,7 +28,15 @@ public class PlayDirector : MonoBehaviour
     [SerializeField] PuyoPair[] nextPuyoPairs = { default!, default! };
 
     NextQueue _nextQueue = new NextQueue();
-    // Start is called before the first frame update
+
+    // 状態管理
+    IState.E_State _current_state = IState.E_State.Control;
+    static readonly IState[] states = new IState[(int)IState.E_State.MAX]
+    {
+        new ControlState(),
+        new GameOverState(),
+    };
+
     void Start()
     {
         _playerController = player.GetComponent<PlayerController>();
@@ -20,8 +44,8 @@ public class PlayDirector : MonoBehaviour
         _playerController.SetLogicalInput(_logicalInput);
 
         _nextQueue.Initialize();
-        Spawn(_nextQueue.Update());
-        UpdateNextsView();
+        // 状態の初期化
+        InitializeState();
     }
 
     void UpdateNextsView()
@@ -65,12 +89,64 @@ public class PlayDirector : MonoBehaviour
         // 入力を取り込む
         UpdateInput();
 
-        if (!player.activeSelf)
+        UpdateState();
+    }
+    class ControlState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
         {
-            Spawn(_nextQueue.Update());
-            UpdateNextsView();
+            if (!parent.Spawn(parent._nextQueue.Update()))
+            {
+                return IState.E_State.GameOver;
+            }
+
+            parent.UpdateNextsView();
+            return IState.E_State.Unchanged;
+        }
+        public IState.E_State Update(PlayDirector parent)
+        {
+            return parent.player.activeSelf ? IState.E_State.Unchanged : IState.E_State.Control;
         }
     }
 
+    class GameOverState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
+        {
+            // リトライ
+            SceneManager.LoadScene(0);
+            return IState.E_State.Unchanged;
+        }
+        public IState.E_State Update(PlayDirector parent)
+        {
+            return IState.E_State.Unchanged;
+        }
+    }
+
+    void InitializeState()
+    {
+        // Debug.Assert(condition: _current_state is >= 0 and < IState.E_State.MAX);
+
+        var next_state = states[(int)_current_state].Initialize(this);
+
+        if (next_state != IState.E_State.Unchanged)
+        {
+            _current_state = next_state;
+            // 初期化で状態が変わるようなら、再帰的に初期を呼び出す
+            InitializeState();
+        }
+    }
+
+    void UpdateState()
+    {
+        var next_state = states[(int)_current_state].Update(this);
+        if (next_state != IState.E_State.Unchanged)
+        {
+            // 次の状態に遷移
+            _current_state = next_state;
+            InitializeState();
+        }
+    }
     bool Spawn(Vector2Int next) => _playerController.Spawn((PuyoType)next[0], (PuyoType)next[1]);
 }
+
