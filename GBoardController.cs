10e62468bfad20829434513917f58bcbc6cd75cf
STD\s20217035 <20217035-nagaishun@hcs.ac.jp>
2023-01-30 14:57:50 +0900

    ちぎり用のコードを追記

--- GBoardController.cs
+++ GBoardController.cs
@@ -2,8 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+struct FallData
+{
+    public readonly int X { get; }
+    public readonly int Y { get; }
+    // 落ちる先
+    public readonly int Dest { get; }
+
+    public FallData(int x, int y, int dest)
+    {
+        X = x;
+        Y = y;
+        Dest = dest;
+    }
+}
 public class BoardController : MonoBehaviour
 {
+    // 単位セル当たりの落下フレーム数
+    public const int FALL_FRAME_PER_CELL = 5;
+
     public const int BOARD_WIDTH = 6;
     public const int BOARD_HEIGHT = 14;
 
@@ -11,6 +28,10 @@ public class BoardController : MonoBehaviour
 
     int[,] _board = new int[BOARD_HEIGHT, BOARD_WIDTH];
     GameObject[,] _Puyos = new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
+
+    // 落ちる際の一次的変数
+    List<FallData> _falls = new List<FallData>();
+    int _fallFrames = 0;
 
     private void ClearAll()
     {
@@ -71,6 +92,16 @@ public class BoardController : MonoBehaviour
         _Puyos[pos.y, pos.x] = Instantiate(prefabPuyo, world_position, Quaternion.identity, transform);
         _Puyos[pos.y, pos.x].GetComponent<Puyo_Controller>().SetPuyoType((PuyoType)val);
 
+        return true;
+    }
+
+    public bool CheckFall()
+    {
+        return true;
+    }
+
+    public bool Fall()
+    {
         return true;
     }
 }
